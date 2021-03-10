43746abaa993bcd24058afed615143e8af1afed9
Rajaram Joshi <rjoshi@deqode.com>
2021-03-10 18:27:07 +0530

    added interface

---
+++ language_detectors/go/pre_detect/v_1_x.go
@@ -0,0 +1,1 @@
+package pre_detect
---
+++ language_detectors/interfaces.go
@@ -0,0 +1,17 @@
+package language_detectors
+
+import "code-analyser/protos"
+
+type LanguageSpecificDetector interface {
+	DetectRuntime(string) string                        //root
+	RunPreDetect(string, string) bool                   //root,version
+	RunParsers(string, string) interface{}              //root version TODO: interface ?
+	ParseENVs(string) []*protos.Output                  //root
+	DetectFrameworks(string, string) []*protos.Output   //root,version
+	DetectDBs(string, string) []*protos.Output          //root,version
+	DetectORMs(string, string) []*protos.Output         //root,version
+	DetectDependencies(string, string) []*protos.Output //root,version
+	DetectLibraries(string, string) []*protos.Output    //root,version
+	GetStaticAssets(string, string) []*protos.Output    //root,version
+	GetStack(string, string) []*protos.Output           //root,version
+}
