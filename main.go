54b9961629779e729b0ee3087db073933966384d
Rajaram Joshi <rjoshi@deqode.com>
2021-03-30 16:14:01 +0530

    map issue resolved

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"code-analyser/analyser"
 	"code-analyser/language_detectors"
 	"code-analyser/runners"
+	"log"
 )
 
 func main() {
@@ -13,9 +14,11 @@ func main() {
 
 func Scrape(path string) {
 	languages, _, _ := analyser.Analyse(path)
+	log.Println(languages)
 	for _, language := range languages {
-		languageDetector := language_detectors.MapLanguageToDetectors[language.Name]
-		runtimeVersion, _ := languageDetector.DetectRuntime(nil, path)
-		runners.RunDetectors(languageDetector, runtimeVersion, path)
+		if languageDetector, ok := language_detectors.MapLanguageToDetectors[language.Name]; ok {
+			runtimeVersion, _ := languageDetector.DetectRuntime(nil, path)
+			runners.RunDetectors(languageDetector, runtimeVersion, path)
+		}
 	}
 }
