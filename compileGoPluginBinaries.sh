b661749f67c2feb9af84cfacd52ecef0b9b895b0
Rajaram Joshi <rjoshi@deqode.com>
2021-04-07 19:02:12 +0530

    libraries

--- compileGoPluginBinaries.sh
+++ compileGoPluginBinaries.sh
@@ -6,6 +6,7 @@ path[2]="plugin/go/db/postgres/V_1_X"
 path[3]="plugin/go/framework/beego/v1_x"
 path[4]="plugin/go/getDependencies"
 path[5]="plugin/go/orm/gorm/V_1_X"
+path[6]="plugin/go/libraries/kafka/V1X"
 
 for p in "${path[@]}"; do
     rm ${p}/plugin -f
