aef3cd3775d0fe68146b65a72c64fb09c2cf40d1
kishan <kchaurasiya@deqode.com>
2021-05-19 12:09:36 +0530

    added test in makefile

---
+++ testingRepos/parsedDependencies/testRepo1/index.js
@@ -0,0 +1,3 @@
+const express = require("express");
+const redis = require("redis");
+const pg = require("postgres");
---
+++ testingRepos/parsedDependencies/testRepo2/index.js
@@ -0,0 +1,4 @@
+const mariadb = require("mariadb");
+const nest = require("@nestjs/core");
+const react = require("react");
+const typeorm = require("typeorm");
